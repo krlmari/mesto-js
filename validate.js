a5f1b5d0b158ecd986f4fd36d19e3791d6eba9b6
krlmari <krylova_m_v@inbox.ru>
2021-05-07 14:39:17 +0300

    Update code

---
+++ validate.js
@@ -0,0 +1,8 @@
+enableValidation({
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__save-button',
+  inactiveButtonClass: 'form__save-button_inactive',
+  inputErrorClass: 'form__input_type_error',
+  errorActiveClass: 'form__input-error_active'
+}); 
